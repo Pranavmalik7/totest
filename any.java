74d6b1ff0a1e604213139eb06cc45fef46299399
Saumya-Purohit <106332064+Saumya-Purohit@users.noreply.github.com>
2022-11-29 10:26:53 +0530

    Update any.java
    updated my n ame

--- any.java
+++ any.java
@@ -2,6 +2,7 @@ public class any {
     public static void main(String args[]){
         System.out.println("hello world");
          System.out.println("hI a m pranav");
+         System.out.println("hI a m saumya");
     }
     
 }
